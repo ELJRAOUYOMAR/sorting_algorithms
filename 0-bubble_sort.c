1623714f48bf58558af755ce2dcfd639628c9239
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-28 10:17:18 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,6 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-<<<<<<< HEAD
     if (!array || !size)
         return;
     
@@ -29,22 +28,3 @@ void bubble_sort(int *array, size_t size)
         }
     }
 }
-=======
-	if (!array || !size)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-			}
-		}
-	}
-}
->>>>>>> 363322f59fdb2b065cad945b05b46dc5d5a345c5
