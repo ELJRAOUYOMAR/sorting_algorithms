a9590910be6e850dbd866801230a32dd7cc48371
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-29 11:40:53 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,28 +7,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+size_t i, j;
+int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap array[j] and array[j+1] */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-        /* If no two elements were swapped in the inner loop, then the array is sorted */
-        if (swapped == 0)
-            break;
-    }
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
 
